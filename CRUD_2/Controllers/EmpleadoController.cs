6681247faaba3fc6e050bf5df73eb1f1da6a7175
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-04-27 11:28:45 -0300

    Agregamos EmpleadoController, vista Index, EmpleadoViewModel

---
+++ CRUD_2/Controllers/EmpleadoController.cs
@@ -0,0 +1,40 @@
+﻿using CRUD_2.Data;
+using CRUD_2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_2.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        private readonly EmpleadoDbContext context;
+
+        public EmpleadoController(EmpleadoDbContext Context)
+        {
+            context = Context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var listaEmpleados = context.Empleados.ToList();
+            List<EmpleadoViewModel> listaEmpViewModel = new List<EmpleadoViewModel>();
+            if (listaEmpleados is not null)
+            {                
+                foreach (var empl in listaEmpleados)
+                {
+                    EmpleadoViewModel modelo = new EmpleadoViewModel()
+                    {
+                        Id = empl.Id,
+                        Nombre = empl.Nombre,
+                        Apellido = empl.Apellido,
+                        Email = empl.Email,
+                        FechaNacimiento = empl.FechaNacimiento,
+                        Salario = empl.Salario,
+                    };
+                    listaEmpViewModel.Add(modelo);                    
+                }
+                return View(listaEmpViewModel);
+            }
+            return View();            
+        }
+    }
+}
---
+++ CRUD_2/Models/EmpleadoViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace CRUD_2.Models
+{
+    public class EmpleadoViewModel
+    {               
+        public int Id { get; set; }        
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        [DisplayName("Fecha de Nacimiento")]
+        public DateTime FechaNacimiento { get; set; }
+        [DisplayName("E-mail")]
+        public string Email { get; set; }
+        public double Salario { get; set; }
+        [DisplayName("Nombre completo")]
+        public string NombreCompleto
+        {
+            get { return Nombre + " " + Apellido; }
+        }
+    }
+}
