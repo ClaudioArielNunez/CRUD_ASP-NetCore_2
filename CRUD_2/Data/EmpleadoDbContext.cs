e57b7748918068f32a396143c602b85cf3bafcbe
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-04-26 21:49:45 -0300

    Agregar archivos de proyecto.

---
+++ CRUD_2/Data/EmpleadoDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_2.Data
+{
+    public class EmpleadoDbContext : DbContext
+    {
+        public EmpleadoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<EmpleadoDbContext> Empleados { get; set; }
+    }
+}
---
+++ CRUD_2/Models/Entidades/Empleado.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUD_2.Models.Entidades
+{
+    public class Empleado
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Column(TypeName ="varchar(50)")]
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Email { get; set; }
+        public double Salario { get; set; }
+    }
+}
